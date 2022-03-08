83adf6037829267bdb7d4ee0082726a36827a46d
Jennifer Okafor <jennakafor00@gmail.com>
2022-03-08 16:37:52 +0100

    updating navigation design

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,15 @@
-let closedMenu = document.querySelector('menu');
+let openMenu = document.querySelector('.open-menu');
+let closeMenu = document.querySelector('.close-menu');
+let mobileNav = document.querySelector('.mobile-active');
 
-classToggle = () => {
+// event listeners for open & close menu icons
+openMenu.addEventListener('click', function () {
+    mobileNav.classList.toggle('mobile-active-show');
+    document.body.style.overflow = 'hidden';
+})
 
-}
+
+closeMenu.addEventListener('click', function () {
+    mobileNav.classList.remove('mobile-active-show');
+    document.body.style.overflow = 'initial';
+})
